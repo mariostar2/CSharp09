dbddfcb69599e078a1719afae2629fbd38e787b5
KIM <alskfl7777@naver.com>
2022-10-26 15:56:24 +0900

    델리게이트 및 일반화 델리게이트

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace CSharp09
+{
+    internal class Program
+    {
+
+        // 델리케이트
+        //=> 함수를 담을수 있는 형식(=자료형)이다.
+
+
+        delegate void CallEvent();                      //반환형이 없고 매개변수가 없는 함수를 담는 델리게이트 자료형.
+        delegate int CallEvent2(int a , int b);        //int를 반환하고  int를 2개 받는 함수를 담는 델리게이트 자료형.
+        static void Main(string[] args)
+        {
+            Sort sort = new Sort();
+                sort.start();
+
+            return;
+
+            CallEvent onCall = Temp;        //onCall 델리게이트에 Temp함수 대입.
+            //onCall = Sum                  //다른 형태의 함수는 대입할 수 없다
+            onCall();                       //델리게이트를 이용하여 함수를 호출함 
+
+            CallEvent2 onCall2 = Sum;          //형식이 맞는 sum을 대입한다     
+            int value = onCall2(10, 20);       // 대리자(deletgate)를 통하여 함수 호출
+            Console.WriteLine($"결과는 : {value}");
+            
+            onCall2 = Minus;
+            value = onCall2(10, 20);
+            Console.WriteLine($"결과는 : {value}");
+            
+            onCall2 = Multiple;
+            value = onCall2(10, 20);
+            Console.WriteLine($"결과는 : {value}");
+
+            Calculate(20, 40, Sum);
+            Calculate(20, 40, Minus);
+            Calculate(20, 40, Multiple);
+        
+        }
+
+        //계산 함수
+        delegate int CalculateEvent(int a, int b);
+        static void Calculate(int a , int b , CalculateEvent onEvent)
+        {
+            int value = onEvent(a, b);
+            Console.WriteLine($"계산 결과는  : {value}다");
+        }
+
+        //void :반환형이 없다.
+        //Temp함수는 반환형이 없고 매개변수를 받지 않는 함수다.
+        static void Temp()
+        {
+            Console.WriteLine("함수 1");
+        }
+
+        //Sum 함수는  int를 반환하고 매개변수  int를 2개 받는다.
+        static int Sum(int a , int b)
+        {
+            return a + b;
+        }
+        
+        static int Minus(int a , int b)
+        {
+            return a - b;
+        }
+
+        static int Multiple(int num1, int num2)
+        {
+            return (num1 * num2)+(num1 * num2);
+        }
+    }
+}
---
+++ Sort.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp09
+{
+    //일반화 델리게이트
+    delegate int Compare<T>(T a, T b);
+
+    internal class Sort
+
+    {
+        public void start()
+        {
+            int[] array = { 10, 18, 5, 7,21 };
+                       
+            Console.WriteLine($"정렬전 : {string.Join(',',array)}");
+            BubbleSort(array, Compare);
+            Console.WriteLine($"정렬후 : {string.Join(',',array)}");
+                
+            string[] array2 = { "AA", "BB", "DD", "CC", "EE" };
+            
+            Console.WriteLine($"정렬전 : {string.Join(',',array2)}");
+            BubbleSort(array2, Compare);            
+            Console.WriteLine($"정렬후 : {string.Join(',',array2)}");
+           
+
+            
+        }
+        
+        //COMPARE 인트타입을 두개받는 델리게이트
+        void BubbleSort<T>(T[] array, Compare<T> compare, bool isAscending = true)
+        {
+            int count = array.Length - 1;
+            int com = isAscending ? -1 : 1;
+           
+            for(int loop = 0; loop < array.Length-1; loop++)
+            {
+                for (int i = 0; i < count -loop; i++) 
+                {
+                    if (compare(array[i],array[i + 1]) == com)
+                        Swap(array, i, i + 1);
+                }
+            }          
+        }
+         
+       void Swap<T>(T[]array, int a ,int b)
+        {
+            int temp = array[a];
+            array[a] = array[b];
+            array[b] = temp;
+        }
+       int CompareInt(int a , int b) {
+            {
+                if (a > b)
+                    return -1;
+                else if (a < b)
+                    return 1;
+
+                else
+                    return 0;
+            } 
+        int Compare(string a ,string b)
+        {
+            int aNum = 0;
+            int bNum = 0;
+
+            foreach (char c in a)
+                aNum += c; 
+                
+            foreach (char c in b)
+                bNum += c;
+
+            if (aNum > bNum)
+                return -1;
+
+            else if (aNum < bNum)
+                return 1;
+            else
+                return 0;
+        }
+        }
+    }
+}
